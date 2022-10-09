c2a14884a78a54f312727e0310e94fe37aa247a9
Alex Deeley <aedeeley@gmail.com>
2022-10-08 19:37:23 -0600

    Fix background
    - Removed svg code
    - Added jpg background image
    - Adjusted css/index for bg

--- index.php
+++ index.php
@@ -10,17 +10,11 @@
 
 <body>
 
-    <div id="background"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 739">
-            <polygon points="0 0 612 0 0 739 0 0" style="fill:#333;" />
-        </svg>
-    </div>
-
     <div id="todo">
         <div id="add">
             <form method="POST" class="formtodo" action="add_query.php">
                 <div id="submit">
-                    <input type="text" class="addtodo" maxlength="200" name="task" placeholder="Add new task"
-                        required />
+                    <input type="text" class="addtodo" maxlength="200" name="task" placeholder="Add new task" required />
                     <button class="btn" name="add">&gt;</button>
                 </div>
         </div>
@@ -32,40 +26,38 @@
             <ul>
 
                 <?php
-					require 'conn.php';
-					$query = $conn->query("SELECT * FROM `task` ORDER BY `task_id` DESC");
-					$count = 1;
-					while($fetch = $query->fetch_array()){
-				?>
-                <li>
-                    <?php
-								if($fetch['status'] != "Done"){
-									echo 
-									'<a href="update_task.php?task_id='.$fetch['task_id'].'&status=Incomplete"><img src="img/check-off.png" /></span></a>';
-								}
-								else {
-									echo '<a href="update_task.php?task_id='.$fetch['task_id'].'&status=Done"><img src="img/check-on.png" /></span></a>';
-								}
-							?>
+                require 'conn.php';
+                $query = $conn->query("SELECT * FROM `task` ORDER BY `task_id` DESC");
+                $count = 1;
+                while ($fetch = $query->fetch_array()) {
+                ?>
+                    <li>
+                        <?php
+                        if ($fetch['status'] != "Done") {
+                            echo
+                            '<a href="update_task.php?task_id=' . $fetch['task_id'] . '&status=Incomplete"><img src="img/check-off.png" /></span></a>';
+                        } else {
+                            echo '<a href="update_task.php?task_id=' . $fetch['task_id'] . '&status=Done"><img src="img/check-on.png" /></span></a>';
+                        }
+                        ?>
 
-                    <label for="check"><span>
-                            <?php 
-					if($fetch['status'] != "Done"){
-						echo $fetch['task'];
-					}
-					else {
-						echo '<strike>' . $fetch['task'] . '</strike>';
-					}
+                        <label for="check"><span>
+                                <?php
+                                if ($fetch['status'] != "Done") {
+                                    echo $fetch['task'];
+                                } else {
+                                    echo '<strike>' . $fetch['task'] . '</strike>';
+                                }
 
-					?></span>
+                                ?></span>
 
-                        <a href="delete_query.php?task_id=<?php echo $fetch['task_id']?>" class="delete">x</a>
+                            <a href="delete_query.php?task_id=<?php echo $fetch['task_id'] ?>" class="delete">x</a>
 
-                    </label>
-                </li>
+                        </label>
+                    </li>
                 <?php
-					}
-				?>
+                }
+                ?>
             </ul>
         </div>
     </div>
